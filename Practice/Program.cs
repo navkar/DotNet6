fac960f6dd26d5f97222721daa30c0a2eb6003e6
naveenkaram <navkar@outlook.com>
2017-03-22 19:06:06 -0500

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                for (int cnt = args.Length - 1; cnt >= 0; cnt--)
+                {
+                    Console.Write(args[cnt] + " ");
+                }
+            }
+
+            //Example();
+
+            //string result = ReadLine();
+            //WriteLine("You typed: " + result);
+
+            PrintMe("This is an expression bodied method");
+
+            Console.WriteLine("\nPress enter to exit");
+            Console.ReadLine();
+        }
+
+        static async void Example()
+        {
+            // This method runs asynchronously.
+            int t = await Task.Run(() => Allocate());
+            Console.WriteLine("Compute: " + t);
+        }
+
+        static void PrintMe(string message) => WriteLine(message);
+        
+        static int Allocate()
+        {
+            // Compute total count of digits in strings.
+            int size = 0;
+            for (int z = 0; z < 100; z++)
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    string value = i.ToString();
+                    if (value == null)
+                    {
+                        return 0;
+                    }
+                    size += value.Length;
+                }
+            }
+            return size;
+        }
+    }
+}
