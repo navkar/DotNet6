abc47db74b5d2ac941ae7e50e468b5a97480673b
naveenkaram <navkar@outlook.com>
2017-03-22 19:14:11 -0500

    Just checking in!

---
+++ Practice/ExpressionBodiedMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    /// <summary>
+    /// A method that includes just one statement can be written in lambda syntax.
+    /// </summary>
+    class ExpressionBodiedMethod
+    {
+        public void PrintMe(string message) => Console.Write("message");
+    }
+}
